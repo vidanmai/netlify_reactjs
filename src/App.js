df3942233c1a180352e2802f6d6f2edbc17efb86
UbuntuDan <vdmai@ifi.uio.no>
2020-07-21 13:50:18 +0200

    Added material-ui AppBar and HomePage component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from '@material-ui/core';
-
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Button, AppBar, Toolbar, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
+  const navStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  const classes = navStyles();
+
+  const navBar = (
+    <div className={classes.root}>
+        <AppBar position="static" style={{backgroundColor: "#4caf50"}}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h4" className={classes.title}>
+              Todo App
+            </Typography>
+            <Button variant="contained" color="secondary">login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My awesome React App
-        </p>
-        <Button variant="contained" color="secondary">Hello World</Button>
-      </header>
+      {navBar}
+      <Typography variant="h1">
+        React App
+      </Typography>
+      <hr/>
+      <Button variant="contained" color="secondary">
+        Hello World!
+      </Button>
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+
+class HomePage extends React.Component {
+    render() {
+        return <h1>[HOME PAGE]</h1>;
+    }
+}
+
+export default HomePage;
