8beadc7a7a675fd0e33aa8bea8c7227f1acabbc9
vidanmai <vidanmai@gmail.com>
2020-08-25 20:26:58 +0200

    Redesigned homepage and added avdelinger to Contact

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "../style/Home.css";
-import bambooSagene from "../img/bambooOutside.PNG";
+//import bambooSagene from "../img/bambooOutside.PNG";
 import foodPic from '../img/bamboo_sushi_bg.jpg';
+import sushiLarge from '../img/bamboo_bg_3.jpg';
 import { Link } from 'react-router-dom';
+import Contact from '../components/Contact';
 
 class Home extends React.Component {
   render() {
-    const bambooInfo = <p>
-      Bamboo Restaurant holder til i Stockfleths gate 53 b på Sagene.
-      Her kan du oppleve det beste fra ulike verdener, sushi og
-      sashimi, samt forskjellige asiatiske retter. 
-      <br / >
-        <br / >
-        Du er velkommen til å nyte rettene på vår restaurant i
-      avslappende og trendy omgivelser, eller bestille takeaway. Vi
-      har også alle rettighetene til å drikke på vår restaurant.
-    </p>
     return (
       <div className="case">
         <div className="infoBanner">
@@ -26,15 +18,17 @@ class Home extends React.Component {
         </div>
         <div className="landingPage">
           <div className="landingImg">
-            <img src={foodPic} className="landingImg" alt=""/>
+            <img src={foodPic} className="landingImg" alt="" />
           </div>
           <div className="landingInfo">
             <div className="info">
-              <h2>Åpningstider: </h2>
+              <h2 className="orangeBorder">Bamboo Sagene</h2>
+              <br />
+              <h3>Åpningstider: </h3>
               <p>
-                <br/>
-                Mandag - Fredag: 
-                <br/>
+                <br />
+                Mandag - Fredag:
+                <br />
                 12:00 - 22:00
                 <br />
                 <br />
@@ -50,25 +44,24 @@ class Home extends React.Component {
             </div>
           </div>
         </div>
+        <div className="secondGrid">
+          <div className="bestillInfo">
+            <h3>Vil du bestille og hente mat?</h3>
+            <Link to="/shop">
+              <button className="bambooButton">Bestill mat</button>
+            </Link>
+          </div>
+          <div className="secondGridImg">
+            <img src={sushiLarge} className="responsiveImg" alt="Sushi meny nr.57" />
+          </div>
+        </div>
         <div className="avdelinger">
-          <div className="avdelingHeader">
-            <h1>Våre Avdelinger</h1>
-            <br />
-            <hr />
-            <br />
-            <div className="avdelingGrid">
-              <div className="buttons">
-                <button className="bambooButton2">Bamboo Sagene</button>
-                <button className="bambooButton2">Bamboo Bjerke</button>
-                <button className="bambooButton2">Bamboo Uranienborg</button>
-              </div>
-              <div className="avdelingInfo">
-                <img className="responsiveImg" src={bambooSagene} alt="" />
-                <div>
-                  {bambooInfo}
-                </div>
-              </div>
-            </div>
+          <h1 className="dark-grey-text">Våre Avdelinger</h1>
+          <br />
+          <hr />
+          <br />
+          <div className="avdelingGrid">
+            <Contact />
           </div>
         </div>
       </div>
