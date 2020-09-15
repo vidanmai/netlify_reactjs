0494546fdfb4fb10ec33d4a092df42c4ad662e30
vidanmai <vidanmai@gmail.com>
2020-09-15 09:48:29 +0200

    made navbar responsive for landscape on mobile

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,21 +6,21 @@ class Info extends React.Component {
             <div className="case">
                 <div className="container">
                     <h1 className="dark-grey-text">Bamboo</h1>
-                    <br/>
-                        <span className="grey-text">
-                            <p>
-                            Bamboo Restaurant holder til i Stockfleths gate 53b på sagene. 
-                            Her kan du oppleve det beste fra både verdener, sushi og sashimi, 
+                    <br />
+                    <span className="grey-text">
+                        <p>
+                            Bamboo Restaurant holder til i Stockfleths gate 53b på sagene.
+                            Her kan du oppleve det beste fra både verdener, sushi og sashimi,
                             samt forskjellige asiatiske retter.
                             </p>
-                            <img className="wideImg" src={bambooPic} alt="" width="100%"/>
-                            <p>
+                        <img className="wideImg" src={bambooPic} alt="" width="100%" />
+                        <p>
                             Du er velkommen til å nyte rettene på vår restaurant i avslappende og trendy omgivelser, eller bestille takeaway.
                             </p>
-                        </span>
+                    </span>
                 </div>
             </div>
-            
+
         );
     }
 }
